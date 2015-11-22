3213b94021802e0e0c3fdc0242d71429a14ef193
BenWu <bw96412@hotmail.com>
2015-11-21 22:36:33 -0500

    set up listview and onclicklistener for it

--- app/src/main/java/benwu/weatherapp/ui/MainActivity.java
+++ app/src/main/java/benwu/weatherapp/ui/MainActivity.java
@@ -1,27 +1,23 @@
 package benwu.weatherapp.ui;
 
-import android.app.LoaderManager;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import benwu.weatherapp.R;
 import benwu.weatherapp.data.SearchHistoryContract;
 import benwu.weatherapp.data.SearchHistoryDbHelper;
 import benwu.weatherapp.utils.DateUtils;
+import static benwu.weatherapp.utils.LogUtils.LOGI;
 import benwu.weatherapp.utils.NetworkHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,16 +33,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDbHelper = new SearchHistoryDbHelper(this);
+
+        // Setting up list view
         String[] fromColumns = SearchHistoryContract.SearchHistoryColumns.PROJ;
         int[] toViews = {R.id.idList, R.id.locationList, R.id.countryList, R.id.dateList};
 
         ListView historyList = (ListView) findViewById(R.id.searchHistory);
+        historyList.setOnItemClickListener(listClickListener);
         mAdapter = new SimpleCursorAdapter(this, R.layout.list_search_history, null, fromColumns, toViews);
 
         historyList.setAdapter(mAdapter);
+    }
 
-        mDbHelper = new SearchHistoryDbHelper(this);
-        Log.i(TAG, cursorToString(mDbHelper.query()));
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // query should happen off main thread for best performance
+        mCursor = mDbHelper.query();
+        mAdapter.swapCursor(mCursor);
+
+        LOGI(TAG, "Cursor: " + cursorToString(mCursor));
     }
 
     public void showWeatherInfo(View view) {
@@ -61,11 +69,26 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Please enter a location", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        mDbHelper.insert(location, country, DateUtils.getCurrentTime());
+
+        startInfoActivity(location, country);
+    }
+
+    private AdapterView.OnItemClickListener listClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String location = ((TextView) view.findViewById(R.id.locationList)).getText().toString();
+            String country = ((TextView) view.findViewById(R.id.countryList)).getText().toString();
+
+            startInfoActivity(location, country);
+        }
+    };
+
+    private void startInfoActivity(String location, String country) {
         Intent intent = new Intent(this, WeatherInfoActivity.class);
         intent.putExtra(WeatherInfoActivity.KEY_LOCATION, location);
         intent.putExtra(WeatherInfoActivity.KEY_COUNTRY, country);
-
-        mDbHelper.insert(location, country, DateUtils.getCurrentTime());
 
         startActivity(intent);
     }
