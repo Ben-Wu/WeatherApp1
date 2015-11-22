77106ab464163fc5f109958c24160c7f0a387aa1
BenWu <bw96412@hotmail.com>
2015-11-22 17:51:13 -0500

    fixed logutils check

--- app/src/main/java/benwu/weatherapp/utils/LogUtils.java
+++ app/src/main/java/benwu/weatherapp/utils/LogUtils.java
@@ -6,29 +6,35 @@ import android.util.Log;
  * Created by Ben Wu on 11/14/2015.
  */
 public class LogUtils {
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     public static void LOGV(String tag, String msg) {
-        Log.v(tag, msg);
+        if(DEBUG)
+            Log.v(tag, msg);
     }
 
     public static void LOGD(String tag, String msg) {
-        Log.d(tag, msg);
+        if(DEBUG)
+            Log.d(tag, msg);
     }
 
     public static void LOGI(String tag, String msg) {
-        Log.i(tag, msg);
+        if(DEBUG)
+            Log.i(tag, msg);
     }
 
     public static void LOGW(String tag, String msg) {
-        Log.w(tag, msg);
+        if(DEBUG)
+            Log.w(tag, msg);
     }
 
     public static void LOGE(String tag, String msg) {
-        Log.e(tag, msg);
+        if(DEBUG)
+            Log.e(tag, msg);
     }
 
     public static void LOGA(String tag, String msg) {
-        Log.wtf(tag, msg);
+        if(DEBUG)
+            Log.wtf(tag, msg);
     }
 }
