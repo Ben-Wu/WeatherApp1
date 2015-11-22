2037cdc2e863bab6995ed133413fea9ba4354c4a
BenWu <bw96412@hotmail.com>
2015-11-21 19:18:38 -0500

    added listview to display search history

--- app/src/main/java/benwu/weatherapp/ui/MainActivity.java
+++ app/src/main/java/benwu/weatherapp/ui/MainActivity.java
@@ -1,16 +1,25 @@
 package benwu.weatherapp.ui;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import benwu.weatherapp.R;
+import benwu.weatherapp.data.SearchHistoryContract;
 import benwu.weatherapp.data.SearchHistoryDbHelper;
 import benwu.weatherapp.utils.DateUtils;
 import benwu.weatherapp.utils.NetworkHelper;
@@ -20,11 +29,21 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     SearchHistoryDbHelper mDbHelper;
+    CursorAdapter mAdapter;
+    Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] fromColumns = SearchHistoryContract.SearchHistoryColumns.PROJ;
+        int[] toViews = {R.id.idList, R.id.locationList, R.id.countryList, R.id.dateList};
+
+        ListView historyList = (ListView) findViewById(R.id.searchHistory);
+        mAdapter = new SimpleCursorAdapter(this, R.layout.list_search_history, null, fromColumns, toViews);
+
+        historyList.setAdapter(mAdapter);
 
         mDbHelper = new SearchHistoryDbHelper(this);
         Log.i(TAG, cursorToString(mDbHelper.query()));
